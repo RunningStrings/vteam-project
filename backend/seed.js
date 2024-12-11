4cb1b6654afd3cd7cc5bc4a537f49717e8ecd8fc
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-12-11 23:42:30 +0100

    Refactor seed process

--- backend/seed.js
+++ backend/seed.js
@@ -16,27 +16,27 @@ const seedData = async () => {
       ];
 
       const bikesData = [
-        { city_id: 'stock', location: { type: 'Point', coordinates: [18.053755700927717,59.33790633117423] }, status: 'charging', battery_level: 20, speed: 0 },
-        { city_id: 'stoc', location: { type: 'Point', coordinates: [18.07143682275389,59.33108277676162] }, status: 'in_use', battery_level: 30, speed: 12 },
-        { city_id: 'lin', location: { type: 'Point', coordinates: [15.618860374560555,58.40981912019322] }, status: 'available', battery_level: 30, speed: 12 },
-        { city_id: 'malmö', location: { type: 'Point', coordinates: [13.005134779931641,55.59986846956199] }, status: 'in_use', battery_level: 30, speed: 12 },
-        { city_id: 'malmö', location: { type: 'Point', coordinates: [13.01388951015625,55.59677094190093,] }, status: 'maintenance', battery_level: 50, speed: 0 }
+        { city_id: 'Stockholm', location: { type: 'Point', coordinates: [18.053755700927717,59.33790633117423] }, status: 'charging', battery_level: 20, speed: 0 },
+        { city_id: 'Stockholm', location: { type: 'Point', coordinates: [18.07143682275389,59.33108277676162] }, status: 'in_use', battery_level: 30, speed: 12 },
+        { city_id: 'Linköping', location: { type: 'Point', coordinates: [15.618860374560555,58.40981912019322] }, status: 'available', battery_level: 30, speed: 12 },
+        { city_id: 'Malmö', location: { type: 'Point', coordinates: [13.005134779931641,55.59986846956199] }, status: 'in_use', battery_level: 30, speed: 12 },
+        { city_id: 'Malmö', location: { type: 'Point', coordinates: [13.01388951015625,55.59677094190093,] }, status: 'maintenance', battery_level: 50, speed: 0 }
       ];
 
       const chargingStationsData = [
-        { city_id: 'stock', location: { type: 'Point', coordinates: [18.053755700927717,59.33790633117423] }, bikes: [], capacity: 10 },
-        { city_id: 'lin', location: { type: 'Point', coordinates: [15.608389030566414,58.4141407833687] }, bikes: [], capacity: 25 },
-        { city_id: 'lin', location: { type: 'Point', coordinates: [15.617143760791024,58.41306744186601,] }, bikes: [], capacity: 12 },
-        { city_id: 'malmö', location: { type: 'Point', coordinates: [13.032943922998047,55.59891074740062] }, bikes: [], capacity: 8 },
-        { city_id: 'malmö', location: { type: 'Point', coordinates: [12.97543736171875,55.58921704103826] }, bikes: [], capacity: 16 }
+        { city_id: 'Stockholm', location: { type: 'Point', coordinates: [18.053755700927717,59.33790633117423] }, bikes: [], capacity: 10 },
+        { city_id: 'Linköping', location: { type: 'Point', coordinates: [15.608389030566414,58.4141407833687] }, bikes: [], capacity: 25 },
+        { city_id: 'Linköping', location: { type: 'Point', coordinates: [15.617143760791024,58.41306744186601,] }, bikes: [], capacity: 12 },
+        { city_id: 'Malmö', location: { type: 'Point', coordinates: [13.032943922998047,55.59891074740062] }, bikes: [], capacity: 8 },
+        { city_id: 'Malmö', location: { type: 'Point', coordinates: [12.97543736171875,55.58921704103826] }, bikes: [], capacity: 16 }
       ];
 
       const parkingZonesData = [
-        { city_id: 'stock', location: { type: 'Polygon', coordinates: [ [18.066412977603,59.34084608742432], [18.06584648145719,59.34088914775366], [18.06627927206204,59.34069537073425], [18.066412977603,59.34084608742432]] } },
-        { city_id: 'stock', location: { type: 'Polygon', coordinates: [ [18.07004381658114,59.33009383540676], [18.07034409689281,59.33016726740666], [18.07000680186237,59.33021133439787], [18.07004381658114,59.33009383540676] ] } },
-        { city_id: 'lin', location: { type: 'Polygon', coordinates: [ [15.61766041309972,58.41061539690683], [15.61766040934009,58.41050051213356], [15.61814290956508,58.41055029045644], [15.61809905144455,58.41066900531456], [15.61766041309972,58.41061539690683] ] } },
-        { city_id: 'malmö', location: { type: 'Polygon', coordinates: [ [12.99746085551704,55.59448009796471], [12.99724331707766,55.59445387919217], [12.99731290270542,55.59387513032012], [12.99752715156276,55.59394732317636], [12.99746085551704,55.59448009796471] ] } },
-        { city_id: 'malmö', location: { type: 'Polygon', coordinates: [ [13.01171500666176,55.59217967678205], [13.01219154425889,55.59210476127265], [13.01238754730634,55.59257116128537], [13.01190178610717,55.5926422931821], [13.01171500666176,55.59217967678205] ] } }
+        { city_id: 'Stockholm', location: { type: 'Polygon', coordinates: [ [18.066412977603,59.34084608742432], [18.06584648145719,59.34088914775366], [18.06627927206204,59.34069537073425], [18.066412977603,59.34084608742432]] } },
+        { city_id: 'Stockholm', location: { type: 'Polygon', coordinates: [ [18.07004381658114,59.33009383540676], [18.07034409689281,59.33016726740666], [18.07000680186237,59.33021133439787], [18.07004381658114,59.33009383540676] ] } },
+        { city_id: 'Linköping', location: { type: 'Polygon', coordinates: [ [15.61766041309972,58.41061539690683], [15.61766040934009,58.41050051213356], [15.61814290956508,58.41055029045644], [15.61809905144455,58.41066900531456], [15.61766041309972,58.41061539690683] ] } },
+        { city_id: 'Malmö', location: { type: 'Polygon', coordinates: [ [12.99746085551704,55.59448009796471], [12.99724331707766,55.59445387919217], [12.99731290270542,55.59387513032012], [12.99752715156276,55.59394732317636], [12.99746085551704,55.59448009796471] ] } },
+        { city_id: 'Malmö', location: { type: 'Polygon', coordinates: [ [13.01171500666176,55.59217967678205], [13.01219154425889,55.59210476127265], [13.01238754730634,55.59257116128537], [13.01190178610717,55.5926422931821], [13.01171500666176,55.59217967678205] ] } }
       ];
 
       const usersData = [
@@ -52,69 +52,28 @@ const seedData = async () => {
         await client.connect();
         const db = client.db('bike_database');
 
-        // Seed cities
-        const cities = db.collection('cities');
-        // Check if the collection has data
-        const citiesCount = await cities.countDocuments();
-        // If the collection is empty, insert data
-        if (citiesCount === 0) {
-            await cities.insertMany(citiesData);
-            console.log('Inserted cities:', cities.insertedIds);
-        } else {
-            console.log('Cities already has data, skipping seed');
-        }
+        // Function to seed collections
+        const seedCollection = async (collectionName, data) => {
+            const collection = db.collection(collectionName);
+            const count = await collection.countDocuments();
+            if (count === 0) {
+                await collection.insertMany(data);
+                console.log(`Inserted ${collectionName}:`, collection.insertedIds);
+            } else {
+                console.log(`${collectionName} already has data, skipping seed`);
+            }
+        };
 
-        // Seed bikes
-        const bikes = db.collection('bikes');
-        // Check if the collection has data
-        const bikesCount = await bikes.countDocuments();
-        // If the collection is empty, insert data
-        if (bikesCount === 0) {
-            await bikes.insertMany(bikesData);
-            console.log('Inserted bikes:', bikes.insertedIds);
-        } else {
-            console.log('Bikes already has data, skipping seed');
-        }
-
-        // Seed charging stations
-        const chargingStations = db.collection('charging_stations');
-        // Check if the collection has data
-        const chargingStationsCount = await chargingStations.countDocuments();
-        // If the collection is empty, insert data
-        if (chargingStationsCount === 0) {
-            await chargingStations.insertMany(chargingStationsData);
-            console.log('Inserted charging stations:', chargingStations.insertedIds);
-        } else {
-            console.log('Charging stations already has data, skipping seed');
-        }
-
-        // Seed parking zones
-        const parkingZones = db.collection('parking_zones');
-        // Check if the collection has data
-        const parkingZonesCount = await parkingZones.countDocuments();
-        // If the collection is empty, insert data
-        if (parkingZonesCount === 0) {
-            await parkingZones.insertMany(parkingZonesData);
-            console.log('Inserted parkingZonesData:', parkingZones.insertedIds);
-        } else {
-            console.log('Parking zones already has data, skipping seed');
-        }
-
-        // Seed users
-        const users = db.collection('users');
-        // Check if the collection has data
-        const usersCount = await users.countDocuments();
-        // If the collection is empty, insert data
-        if (usersCount === 0) {
-            await users.insertMany(usersData);
-            console.log('Inserted parkingZonesData:', users.insertedIds);
-        } else {
-            console.log('Parking zones already has data, skipping seed');
-        }
+        // Seed all the collections
+        await seedCollection('cities', citiesData);
+        await seedCollection('bikes', bikesData);
+        await seedCollection('charging_stations', chargingStationsData);
+        await seedCollection('parking_zones', parkingZonesData);
+        await seedCollection('users', usersData);
 
         console.log('Database seeded!');
     } catch (error) {
-        console.error('Error seeding database:', error);
+        console.error('Error seeding database:', error.message || error);
     } finally {
         await client.close();
     }
