1e7e7deec770fca783dd37d8e8555f95b501fff0
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-12-11 15:12:38 +0100

    Change Saldo to Priser

--- mobileapp-customer/app/(tabs)/_layout.jsx
+++ mobileapp-customer/app/(tabs)/_layout.jsx
@@ -36,7 +36,7 @@ export default function TabLayout() {
         <Tabs.Screen
             name="payment"
             options={{
-            title: 'Saldo',
+            title: 'Priser',
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="creditcard" color={color} />,
             }}
         />
