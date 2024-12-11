05734b7cd9665791b21108ce7e2cac38b2c6b96a
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-12-11 02:24:31 +0100

    Adjust and style tabs
    - Change order of tabs.
    - Add stylesheet for roundTabIcon.
    - Apply style roundTabIcon to scan tab.

--- mobileapp-customer/app/(tabs)/_layout.jsx
+++ mobileapp-customer/app/(tabs)/_layout.jsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, View, StyleSheet } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -29,17 +29,56 @@ export default function TabLayout() {
         <Tabs.Screen
             name="index"
             options={{
-            title: 'Home',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+            title: 'Karta',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="map" color={color} />,
             }}
         />
         <Tabs.Screen
-            name="explore"
+            name="payment"
             options={{
-            title: 'Explore',
-            tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+            title: 'Saldo',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="creditcard" color={color} />,
+            }}
+        />
+        <Tabs.Screen
+            name="scan"
+            options={{
+            title: '',
+            tabBarIcon: ({ color }) => (
+                <View style={styles.roundTabIcon}>
+                <IconSymbol size={56} name="qrcode.viewfinder" color={Colors.light.secondary} />
+            </View>
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name="support"
+            options={{
+            title: 'Support',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="lifepreserver" color={color} />,
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+            title: 'Profil',
+            tabBarIcon: ({ color }) => <IconSymbol size={28} name="person" color={color} />,
             }}
         />
         </Tabs>
     );
 }
+
+const styles = StyleSheet.create({
+    roundTabIcon: {
+        backgroundColor: Colors.light.primary,
+        borderRadius: 50,
+        padding: 35, 
+        borderWidth: 2,
+        borderColor: Colors.light.secondary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 60,
+        height: 60,
+    },
+});
