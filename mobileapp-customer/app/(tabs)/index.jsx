93e84f67874577dfbcb51f45ce33c8fb83d56f69
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-12-11 15:07:20 +0100

    Add some markers

--- mobileapp-customer/app/(tabs)/index.jsx
+++ mobileapp-customer/app/(tabs)/index.jsx
@@ -6,10 +6,12 @@ import * as Location from 'expo-location';
 const locations = [
     { id: 1, latitude: 58.41068698245291, longitude: 15.621581899999995, title: "Hejhej", description: "Här är Folke Filbyter" },
     { id: 2, latitude: 55.705157947051994, longitude: 13.189122134316587, title: "Hallå", description: "Här är Mormors Bageri" },
-    { id: 3, latitude: 55.598096442418644, longitude: 12.997459039597759, title: "Tjoho", description: "Här är Lilla Glassfabriken" }
+    { id: 3, latitude: 55.598096442418644, longitude: 12.997459039597759, title: "Tjoho", description: "Här är Lilla Glassfabriken" },
+    { id: 4, latitude: 59.327605696465035, longitude: 18.074089948642243, title: "Nämen", description: "Här kan man lägga båten" },
+    { id: 5, latitude: 59.85805333537346, longitude: 17.638152751372555, title: "Hoho", description: "Här verkar det finnas bra fika"}
 ];
 
-export default function ShowMap() {
+export default function TabOneScreen() {
     const [region, setRegion] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
 
