03537575dca034b697ac4aa05b59f6164208a523
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-12-12 08:47:35 +0100

    Comment out env

--- backend/database.js
+++ backend/database.js
@@ -5,8 +5,8 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const database = {
     getDb: async function getDb () {
-        // let dsn = `mongodb://mongodb:27017/bike_database`;
-        let dsn = process.env.MONGO_DSN;
+        let dsn = `mongodb://mongodb:27017/bike_database`;
+        // let dsn = process.env.MONGO_DSN;
 
         const client = new MongoClient(dsn, {
             serverApi: {
