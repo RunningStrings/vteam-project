8b6b52f51226d93485fec4d5920ffe15cc59e527
Björn Horndahl <bjorn9111@gmail.com>
2024-12-12 04:21:07 +0100

    Adjust errorcode to json

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ app.get('/users', async (req, res) => {
     res.json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
-    res.status(500).send('Server Error');
+    // res.status(500).send('Server Error');
+    res.status(500).json({ errorMessage: "Server Error" });
   }
 });
 
