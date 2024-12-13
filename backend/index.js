f778b177ecf4233943365dd5e7fa688e487a97a1
Björn Horndahl <bjorn9111@gmail.com>
2024-12-13 13:49:57 +0100

    Create first draft api main program

--- backend/index.js
+++ backend/index.js
@@ -4,30 +4,29 @@
  * @author Bikeriderz
  *
  */
-"use strict";
 
 import express from 'express';
 import database from './database.js';
 
 const app = express();
-const PORT = process.env.PORT || 1337;
+const PORT = process.env.PORT || 5000;
 
-// const routeIndex = require("./route/index.js");
-const userRouter = require("./routes/users")
-const cityRouter = require("./routes/cities")
-const bikeRouter = require("./routes/bikes")
-const stationRouter = require("./routes/stations")
-const zoneRouter = require("./routes/zones")
-const rentRouter = require("./routes/rents")
-const middleware = require("./middleware/index.js");
+// import { router as indexRouter } from './routes/index.js';
+import { router as userRouter } from './routes/users.js';
+// import { router as cityRouter } from './routes/cities.js';
+// import { router as bikeRouter } from './routes/bikes.js';
+// import { router as stationRouter } from './routes/stations.js';
+// import { router as zoneRouter } from './routes/zones.js';
+// import { router as rentRouter } from './routes/rents.js';
+import logIncomingToConsole from './middlewear/index.js';
 
 app.use("/users", userRouter)
-app.use("/cities", cityRouter)
-app.use("/bikes", bikeRouter)
-app.use("/stations", stationRouter)
-app.use("/zones", zoneRouter)
-app.use("/rents", rentRouter)
-app.use(middleware.logIncomingToConsole);
+// app.use("/cities", cityRouter)
+// app.use("/bikes", bikeRouter)
+// app.use("/stations", stationRouter)
+// app.use("/zones", zoneRouter)
+// app.use("/rents", rentRouter)
+app.use(logIncomingToConsole);
 
 app.get('/', (req, res) => {
   res.send('Hello from the Backend!');
@@ -56,7 +55,7 @@ app.get('/bikes', async (req, res) => {
   }
 });
 
-app.listen(port, logStartUpDetailsToConsole);
+app.listen(PORT, logStartUpDetailsToConsole);
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
@@ -85,7 +84,7 @@ function logStartUpDetailsToConsole() {
         }
     });
 
-    console.info(`Server is listening on port ${port}.`);
+    console.info(`Server is listening on port ${PORT}.`);
     console.info("Available routes are:");
     console.info(routes);
 }
