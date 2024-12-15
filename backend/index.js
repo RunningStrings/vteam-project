c042874c35b2acc573d0ec2975305ce9dfbcd6a6
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2024-12-15 15:36:52 +0100

    Add comment about port 5001

--- backend/index.js
+++ backend/index.js
@@ -96,6 +96,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// Using separate port to make it easier to see bike-brain communication.
+// Can be removed when not needed (socket runs on the regular backend port).
 server.listen(5001, () => {
   console.log('Socket.IO server is running on http://localhost:5001');
 });
