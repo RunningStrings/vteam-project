072222b8c719673903099085357440411a32f937
Björn Horndahl <bjorn9111@gmail.com>
2024-12-17 12:58:41 +0100

    Add rules

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -3,16 +3,23 @@ import globals from "globals";
 
 export default [
     js.configs.recommended,
-
    {
        rules: {
            "no-unused-vars": "warn",
-           "no-undef": "warn"
+           "no-undef": "warn",
+           "semi": ["error", "always"],
+        //    "eqeqeq": ["error", "always"],
+        //    "no-console": "warn",
+        //    "quotes": ["error", "double"],
+        //    "curly": ["error", "all"],
+        //    "no-magic-numbers": ["warn", { "ignore": [0, 1] }],
+        //    "consistent-return": "error",
+        //    "prefer-const": "error",
        },
        languageOptions: {
             globals: {
                 ...globals.node,
             }
-        }
-   }
+        },
+   },
 ];
