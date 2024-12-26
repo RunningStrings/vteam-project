ce7c45f0075531e25ea06b7daa97c49e84873c10
Björn Horndahl <bjorn9111@gmail.com>
2024-12-27 00:36:06 +0100

    Make code modular for users route

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,102 @@
 /**
- * Model for users. Stores helper functions for users route.
+ * Model object for users. Stores model functions for users route.
  */
-import database from './../database.js';
+import database from '../database.js';
+import { ObjectId } from 'mongodb';
+import { createError } from './utils/createError.js'
 
 const userModel = {
+    fetchAllUsers: async function fetchAllUsers() {
+        const db = await database.getDb();
+
+        try {
+            const result = await db.collectionUsers.find().toArray();
+
+            return result;
+        } catch (error) {
+            createError(error);
+        } finally {
+            await db.client.close();
+        }
+    },
+
     fetchUserById: async function fetchUserById(id) {
+        if (!ObjectId.isValid(id)) {
+            createError("ID format is invalid", 400)
+            // return res.status(400).json({ errorMessage: "ID format is invalid" });
+        }
+
         const db = await database.getDb();
 
         try {
             const filter = {
-                id: id
+                // $and: [
+                //     // {
+                //     //     $or: [
+                //         //         { owner: user.email },
+                //         //         { collaborators: user.email }
+                //         //     ]
+                //         // },
+                //         { _id: ObjectId.createFromHexString(id) }
+                //     ]
+                _id: ObjectId.createFromHexString(id)
             };
-
+                
             const result = await db.collectionUsers.findOne(filter);
 
+            if (!result) {
+                createError(`user with ID: ${id} cannot be found`, 404);
+                // return res.status(404).json({ errorMessage: "User can not be found.",
+                //     objectId: userId
+                // }
+                // );
+            }
+
             return result;
-        } catch (error) {
-            throw new Error(error.message ? error.message : "Database error");
+        } finally {
+            await db.client.close();
+        }
+    },
+
+    updateUserById: async function updateUserById(id, body) {
+        if (!ObjectId.isValid(id)) {
+            createError("ID format is invalid", 400)
+        }
+
+        const db = await database.getDb();
+
+        try {
+            const filter = {
+                // $and: [
+                //     // {
+                //     //     $or: [
+                //         //         { owner: user.email },
+                //         //         { collaborators: user.email }
+                //         //     ]
+                //         // },
+                //         { _id: ObjectId.createFromHexString(id) }
+                //     ]
+                _id: ObjectId.createFromHexString(id)
+                };
+                
+            let result = await db.collectionUsers.findOne(filter);
+
+            if (!result) {
+                createError(`user with ID: ${id} cannot be found`, 404);
+            }
+
+            result = await db.collectionUsers.updateOne(
+                { _id: ObjectId.createFromHexString(id) },
+                { $set: body }
+            );
+
+            if (result.modifiedCount !== 1) {
+                createError(`Fail. No update possible with the given information for user with ID: ${id}.`, 400);
+            }
+
+            result = await db.collectionUsers.findOne(filter);
+
+            return result;
         } finally {
             await db.client.close();
         }
