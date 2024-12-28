9c0ce3d9373fcd5a09849f8e36c3351749fea869
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-12-28 23:23:44 +0100

    Reorganize files

---
+++ backend/data_generation/generateData.js
@@ -0,0 +1,5 @@
+import path from 'node:path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'fs/promises';
+import { faker, fakerSV } from '@faker-js/faker';
