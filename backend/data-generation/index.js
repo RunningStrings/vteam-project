93f735dafd1dddae5630c300bf2de58d98bd5871
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-12-29 18:38:27 +0100

    Add users generation data

--- backend/data-generation/index.js
+++ backend/data-generation/index.js
@@ -1,4 +1,6 @@
 import updateIds from './updateIds.js';
+import generateUserJsonFile from './generateUsers.js';
+
 
 /**
  * Main function to generate and update data.
@@ -6,6 +8,11 @@ import updateIds from './updateIds.js';
 const main = () => {
     // Update or create Id and increment it for parkings and stations
     updateIds(['../data-json/parkings', '../data-json/stations']);
+
+    // Generate 50 users
+    generateUserJsonFile('../data-json/users.json', 50)
+    // Update their id
+    updateIds(['../data-json/users.json']);
 };
 
 main();
