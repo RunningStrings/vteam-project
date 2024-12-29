3128653d98d7a4a7b3033607a9d091b7cb6b0d41
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-12-29 16:57:10 +0100

    Refactorate function into a main function with imports

--- backend/data_generation/index.js
+++ backend/data_generation/index.js
@@ -1,4 +1,11 @@
 import updateIds from './updateIds.js';
 
-// Update Id and increment it for parkings and stations
-updateIds(['../data_json/parkings', '../data_json/stations']);
+/**
+ * Main function to generate and update data.
+ */
+const main = () => {
+    // Update or create Id and increment it for parkings and stations
+    updateIds(['../data_json/parkings', '../data_json/stations']);
+};
+
+main();
