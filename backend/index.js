1dae759c6a11944cd8fe8f1d624a190e4d56e421
Björn Horndahl <bjorn9111@gmail.com>
2025-01-06 12:09:40 +0100

    Revert "Revert "Add import trips""
    This reverts commit 65d340f3c197d26a1b5c6917013f52aaa91de0a3.

--- backend/index.js
+++ backend/index.js
@@ -17,16 +17,17 @@ import { router as cityRouter } from './routes/cities.js';
 import { router as bikeRouter } from './routes/bikes.js';
 import { router as stationRouter } from './routes/chargingStations.js';
 import { router as zoneRouter } from './routes/parkingZones.js';
-// import { router as rentRouter } from './routes/rents.js';
+import { router as tripRouter } from './routes/trips.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
 import logIncomingToConsole from './middlewares/index.js';
 
+// const pathV1 = "/api/v1";
 app.use("/users", userRouter);
 app.use("/cities", cityRouter);
 app.use("/bikes", bikeRouter);
 app.use("/charging_stations", stationRouter);
 app.use("/parking_zones", zoneRouter);
-// app.use("/rents", rentRouter)
+app.use("/trips", tripRouter)
 app.use(logIncomingToConsole);
 app.use(errorMiddleware);
 
