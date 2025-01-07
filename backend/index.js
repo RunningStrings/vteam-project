a3b7c8262cd4af7b4f741412d11a60c28dc96a10
Björn Horndahl <bjorn9111@gmail.com>
2025-01-07 12:27:24 +0100

    Update route version to api/v1 and make modular

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,18 @@
  *
  */
 import express from 'express';
-// import database from './database-config/database.js';
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(express.json());
-
-// import { router as indexRouter } from './routes/index.js';
-import { router as userRouter } from './routes/users.js';
-import { router as cityRouter } from './routes/cities.js';
-import { router as bikeRouter } from './routes/bikes.js';
-import { router as stationRouter } from './routes/chargingStations.js';
-import { router as zoneRouter } from './routes/parkingZones.js';
-import { router as tripRouter } from './routes/trips.js';
+import allRoutes from './routes/index.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
 import logIncomingToConsole from './middlewares/index.js';
 
-// const pathV1 = "/api/v1";
-app.use("/users", userRouter);
-app.use("/cities", cityRouter);
-app.use("/bikes", bikeRouter);
-app.use("/charging_stations", stationRouter);
-app.use("/parking_zones", zoneRouter);
-app.use("/trips", tripRouter)
+const app = express();
+const PORT = process.env.PORT || 5000;
+const pathV1 = "/api/v1";
+
+app.use(express.json());
 app.use(logIncomingToConsole);
 app.use(errorMiddleware);
+allRoutes(app, pathV1);
 
 app.get('/', (req, res) => {
   res.send('Hello from the Backend!');
