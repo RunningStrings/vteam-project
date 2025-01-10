6bc6a5d1beedbe690a191afa826cdd477c534d05
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2025-01-10 03:48:05 +0100

    Change handling of API_URL
    - Remove collection name from const API_URL.
    - Add collection name '/bikes' to call on API when fetching bikes.

--- bike-brain/sim-from-database.js
+++ bike-brain/sim-from-database.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 
 const socket = io('http://localhost:5001');
 
-const API_URL = 'http://localhost:5000/api/v1/bikes';
+const API_URL = 'http://localhost:5000/api/v1';
 
 const BATCH_SIZE = 200;
 
