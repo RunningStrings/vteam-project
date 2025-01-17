9f075f28b33fc3295a1ed5ae3c0214b8ff090a35
Björn Horndahl <bjorn9111@gmail.com>
2025-01-17 11:56:21 +0100

    Create token service for generating and verifying tokens

--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -1,36 +1,22 @@
 import 'dotenv/config';
-
 import jwt from 'jsonwebtoken';
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || "bikeriderz12345!";
 
 const tokenService = {
-    generateToken: function generateToken(email) {
-        return jwt.sign(email, secret, { expiresIn: '1h'});
+    generateToken: function generateToken(user) {
+        const token = jwt.sign(user, secret, { expiresIn: '6h'});
+        // console.log('GENERATED TOKEN ');
+        // console.log(token);
+        return token;
+        // return jwt.sign(user, secret, { expiresIn: '6h'});
+    },
+
+    verifyToken: function verifyToken(token) {
+        // console.log('THE TOKEN IN VERIFYTOKEN:');
+        // console.log(token);
+        return jwt.verify(token, secret);
     }
 };
 
 export default tokenService;
-
-
-
-
-
-
-import jwt from 'jsonwebtoken';
-import config from '../config/index.js';
-
-export const generateJWT = (user) => {
-  const payload = {
-    githubId: user.githubId,
-    username: user.username,
-    email: user.email,
-    avatarUrl: user.avatarUrl,
-  };
-
-  return jwt.sign(payload, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRATION });
-};
-
-export const verifyJWT = (token) => {
-  return jwt.verify(token, config.JWT_SECRET);
-};
