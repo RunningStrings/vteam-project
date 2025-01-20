19c0e6ef0bf468342a9cb231dab9209d51a00d98
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2025-01-20 15:23:42 +0100

    Update array for status

--- backend/data-generation/generateBikes.js
+++ backend/data-generation/generateBikes.js
@@ -7,7 +7,7 @@ import { faker } from '@faker-js/faker';
  */
 const generateBike = () => {
     const city = faker.helpers.arrayElement(['Stockholm', 'Linköping', 'Malmö']);
-    const status = faker.helpers.arrayElement(["charging", "available", "maintenance"]); // Random status from array
+    const status = faker.helpers.arrayElement(["available", "maintenance"]); // Random status from array
     const battery = faker.number.int({ min: 0, max: 100 });
     const speed = status === 'in_use' ? faker.number.int({ min: 1, max: 20 }) : 0; // Random speed if in use
 
